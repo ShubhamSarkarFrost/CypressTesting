00b1a34afbe783e824acc43b4cb7989c61c9fff8
Shubham Sarkar <oculusgrudge@gmail.com>
2022-07-26 16:31:48 +0530

    Cypress_First_Commit_26_07_2022_04:31_IST

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,8 @@
+// cypress -Spec file
+describe('My First Test',function () {
+     it('Navigate to Dummy GreenKart Application', function(){
+          cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+          
+          
+     })    
+})
